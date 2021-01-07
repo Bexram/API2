332d89e9e7eefab76c999261090aa9a2388ae33f
bexram <bexram33@mail.ru>
2021-01-07 04:13:26 -0800

    contract issuse

--- api/checklist/views.py
+++ api/checklist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 from rest_framework import generics
 from . import models,serializers

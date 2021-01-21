bce4248207e5b11312fcba02ba2ca3cf56e67631
bexram <bexram33@mail.ru>
2021-01-21 11:13:46 +0300

    getcompany

--- api/checklist/urls.py
+++ api/checklist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('getcompany/', views.CompanyGetList.as_view()),
     path('getchecklist/<int:pk>/', views.TaskGetList.as_view()),
     path('checklist/', views.TaskList.as_view()),
     path('checklist/<int:pk>/', views.TaskDetailList.as_view()),

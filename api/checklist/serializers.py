5535a5ab535a13d83790fd1d97a95279c976c7ab
bexram <bexram33@mail.ru>
2021-01-21 10:40:03 +0300

    modified checklist

--- api/checklist/serializers.py
+++ api/checklist/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task,Company
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('__all__')
+
+class TaskGetSerializer(serializers.ModelSerializer):
+    company=CompanySerializer()
+    class Meta:
+        model = Task
+        fields = ('__all__')
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:

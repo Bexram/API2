25705ce55b531754dd813f5ef1fcf64806807de3
bexram <bexram33@mail.ru>
2021-03-09 16:30:24 +0300

    правки №3 от 07.03 (getauto)

--- api/users/urls.py
+++ api/users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('users/<int:pk>/', views.UserDetailList.as_view()),
     path('auto/', views.AutoList.as_view()),
     path('auto/<int:pk>/', views.AutoDetailList.as_view()),
-    path('getauto/', views.GetAutoList.as_view()),
+    path('getauto/<int:pk>/', views.GetAutoList.as_view()),
     path('units/', views.UnitList.as_view()),
     path('units/<int:pk>/', views.UnitDetailList.as_view()),
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status, generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
 from .serializers import GetUserSerializer, authSerializer, UserProfileSerializer, AutoSerializer, UnitSerializer, GetAutoSerializer
 from .models import UserProfile, auth, Auto, Units
 
@@ -31,34 +31,36 @@ class UserDetailList(generics.RetrieveUpdateDestroyAPIView):
 
 
 class GetUserList(generics.ListCreateAPIView):
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = auth.objects.filter(status=1).order_by('-is_active','userprofile__last_name')
     serializer_class = GetUserSerializer
 
 class AutoList(generics.ListCreateAPIView):
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Auto.objects.all()
     serializer_class = AutoSerializer
 
 
 class AutoDetailList(generics.RetrieveUpdateDestroyAPIView):
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Auto.objects.all()
     serializer_class = AutoSerializer
 
 
 class GetAutoList(generics.ListCreateAPIView):
-    #permission_classes = [IsAuthenticated]
-    queryset = Auto.objects.all()
-    serializer_class = GetAutoSerializer
+    permission_classes = [IsAuthenticated]
+    def get(request, self, pk, format=None):
+        queryset = Auto.objects.filter(userprof=pk)
+        serializer = AutoSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class UnitList(generics.ListCreateAPIView):
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Units.objects.all()
     serializer_class = UnitSerializer
 
 
 class UnitDetailList(generics.RetrieveUpdateDestroyAPIView):
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Units.objects.all()
     serializer_class = UnitSerializer

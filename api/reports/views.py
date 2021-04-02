81870a8dcb5a063fcff74550a5be92210b0d4fc0
bexram <bexram33@mail.ru>
2021-04-02 01:38:45 -0700

    report and crons

--- api/reports/views.py
+++ api/reports/views.py
@@ -4,14 +4,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from . import models, serializers
-import users, clients, tasks, api
+import users, clients, tasks
 import datetime
-from docxcompose.composer import Composer
-from docx import Document as Document_compose
 from django.http import FileResponse
 import os
 import subprocess
-
+from PyPDF2 import PdfFileMerger
 
 def getQuarterStart(dt=datetime.date.today()):
     return datetime.date(dt.year, (dt.month - 1) // 3 * 3 + 1, 1)
@@ -49,7 +47,7 @@ class GetRepList(APIView):
         serializer = serializers.GetReportSerializer(queryset, many=True)
         return Response(serializer.data)
 
-def ConstructDoc(data,name):
+def ConstructDoc(data,docpath,pdfpath):
     doc = DocxTemplate(os.path.abspath('reports/template.docx'))
     context = {'works': data.works,
                'object': data.clientobj.object_name,
@@ -64,11 +62,28 @@ def ConstructDoc(data,name):
                'results': data.results
                }
     doc.render(context)
-    doc.save(name)
+    doc.save(docpath)
+    env = os.environ.copy()
+    env['HOME'] = '/tmp'
+
+    p = subprocess.Popen(["unoconv", "-f", "pdf", "-o", pdfpath,
+                          docpath], env=env)
+    out, err = p.communicate()
+
+def merger(files,output):
+    merger = PdfFileMerger()
+    for pdf in files:
+        merger.append(pdf)
+    merger.write(output)
+    merger.close()
+
+
 
 class GenerateReport(APIView):
     def get(self, request, format=None):
         data = models.QReport.objects.filter(auto_generate=True)
+        docpath='./generated_doc.docx'
+        output='./report.pdf'
         for report in enumerate(data):
             new=models.ReadyReport(
                 clientobj=report[1].clientobj,
@@ -81,31 +96,22 @@ class GenerateReport(APIView):
                 results=report[1].results,
                             )
             new.save()
-            if report[0] == 0:
-                ConstructDoc(report[1],'generated_doc.docx')
-            else:
-                ConstructDoc(report[1],'generated_doc1.docx')
-                master = Document_compose("generated_doc.docx")
-                composer = Composer(master)
-                # filename_second_docx is the name of the second docx file
-                doc2 = Document_compose("generated_doc1.docx")
-                # append the doc2 into the master using composer.append function
-                composer.append(doc2)
-                # Save the combined docx with a name
-                composer.save("generated_doc.docx")
-
-        short_report = open("generated_doc.docx", 'rb')
-        return FileResponse(short_report)
-        # response = HttpResponse(FileWrapper(short_report), content_type='application/msword')
-        # response['Content-Disposition'] = 'attachment; filename= "reports.docx"'
-        # return response
+            ConstructDoc(report[1],docpath,'./files/media/file'+str(report[0])+'.pdf')
+        pdfFiles = []
+        for filename in os.listdir('./files/media'):
+            if filename.endswith('.pdf'):
+                pdfFiles.append('./files/media/'+filename)
+        print(pdfFiles)
+        merger(pdfFiles,output)
+        for file in pdfFiles:
+            os.remove(file)
+        return FileResponse(open(output, 'rb'))
 
 class GenerateOneReport(APIView):
     def get(self, request, pk, format=None):
-        path='./files/media/generated_doc.docx'
-        output="./files/media/report.pdf"
+        path='./generated_doc.docx'
+        output="./output.pdf"
         data = models.QReport.objects.get(id=pk)
-        print(data)
         new=models.ReadyReport(
                 clientobj=data.clientobj,
                 contact_man=data.contact_man,
@@ -117,29 +123,5 @@ class GenerateOneReport(APIView):
                 results=data.results,
                             )
         new.save()
-        ConstructDoc(data,path)
-        # a_url = "https://s3.pdfconvertonline.com:443/convert/p9.php"
-        # a_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
-        #              "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        #              "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate",
-        #              "Content-Type": "multipart/form-data; boundary=---------------------------326089013215643646281334313309",
-        #              "Origin": "https://www.pdfconvertonline.com", "DNT": "1", "Connection": "close",
-        #              "Referer": "https://www.pdfconvertonline.com/docx-to-pdf-online.html",
-        #              "Upgrade-Insecure-Requests": "1"}
-        #
-        # file = open(path, "rb")
-        #
-        # Input_file = file.read()
-        # a = Input_file.decode('latin-1').encode("utf-8")
-        # l = str(a, 'UTF-8')
-        # a_data = "-----------------------------326089013215643646281334313309\r\nContent-Disposition: form-data; name=\"localfile\"; filename=\"generated_doc.docx\"\r\nContent-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document\r\n\r\n" + l + "\r\n-----------------------------326089013215643646281334313309\r\nContent-Disposition: form-data; name=\"filetype\"\r\n\r\nPDF\r\n-----------------------------326089013215643646281334313309\r\nContent-Disposition: form-data; name=\"code\"\r\n\r\n1\r\n-----------------------------326089013215643646281334313309\r\nContent-Disposition: form-data; name=\"source\"\r\n\r\nWEENYSOFT\r\n-----------------------------326089013215643646281334313309\r\nContent-Disposition: form-data; name=\"cengine\"\r\n\r\n2\r\n-----------------------------326089013215643646281334313309--\r\n"
-        # r = requests.post(a_url, headers=a_headers, data=a_data)
-        # result = re.search("value.*.pdf", r.text)
-        env = os.environ.copy()
-        env['HOME'] = '/tmp'
-
-        #p = subprocess.Popen(["unoconv", "-f", "pdf", "-o",output,
-        #                      path], env=env)
-        #out, err = p.communicate()
-
+        ConstructDoc(data,path,output)
         return FileResponse(open(output, 'rb'))
